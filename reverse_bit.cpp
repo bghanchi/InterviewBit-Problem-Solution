b2d280a8e42fb29deaeafba897d95ec70288e94d
Bharat Kumar <bghanchi1997@gmail.com>
2019-07-18 23:06:29 +0530

    Add files via upload

---
+++ reverse_bit.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<string.h>
+#include<tuple>
+#define all(c) c.begin(),c.end()
+#define tr(i,n)  for(int i=0;i<n;i++)
+#define pb push_back
+#define em emplace_back
+#define ll long long int
+#define mp make_pair
+#define mt make_tuple
+#define debug(c) cout<<c<<endl;
+#define find(x,v)  find(all(v),c)!=v.end()
+#define isro ios_base::sync_with_stdio(false);
+
+using namespace std;
+typedef vector<int>vi;
+typedef vector<vi>vii;
+typedef pair<int,int>pi;
+typedef tuple<int,int,int>ti;
+
+
+void reverse_bit(int n){
+//      unsigned long  int a=(1<<);
+      cout<<(1<<32)<<endl;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    reverse_bit(n);
+}
